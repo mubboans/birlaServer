2a433fc69e8ec1db4115cceb51b783011b3bf284
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-04-07 20:25:36 +0530

    sequelize-cli integration done

--- src/route/user_route.js
+++ src/route/user_route.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { createUser } = require('../controller/user_controller');
+const { createUser, getAllUser } = require('../controller/user_controller');
 const route = express.Router();
 
-route.post('/user/create',createUser)
+route.post('/user/create',createUser);
+
+route.get('/user/data/all' , getAllUser);
 
 module.exports = route
