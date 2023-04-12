ab13cb3c1f6ea3dbf0dd6eb9226669abb6bc0f7d
Mubashir Ansari <95461257+mubboans@users.noreply.github.com>
2023-04-12 10:58:12 +0530

    app.js commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
+const path =require('path')
 const connectDB = require('./dbconfig/dbonfig')
-const port = 8000
+const {port}  = require('./config/config')
+
+const apiErrorHandler = require('./error/api-error-handler')
 const userroute = require('./routes/user_route')
+const itemroute = require('./routes/invoice_item_route')
 app.get('/data', (req, res) => {
   res.send('Hello World!')
 })
-
 app.use(express.json());
+app.use('/invoice',itemroute);
 app.use('',userroute) 
+app.use(apiErrorHandler);
+app.use('**/**',function(req, res, next) {
+
+  res.status(404);
+  res.sendFile(path.join(__dirname, './public/404-not-found.html'))
+}
+);
+
 app.listen(port,async () => {
   try {
     // console.log(sequelize);
